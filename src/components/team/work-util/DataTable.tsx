f53aec6cd5fd11790016dc8a21837244f5a03904
Abdul Rahman Al Sbeinaty <61135058+vladiusftw@users.noreply.github.com>
2025-06-16 15:13:14 +0200

    Update DataTable.tsx
    - Add space between input and dropdown

--- src/components/team/work-util/DataTable.tsx
+++ src/components/team/work-util/DataTable.tsx
@@ -83,7 +83,7 @@ export function DataTable<TData, TValue>({
 
     return (
         <div>
-            <div className="flex items-center pb-4">
+            <div className="flex items-center pb-4 space-x-2">
                 <Input
                     placeholder="Filter emails..."
                     value={
